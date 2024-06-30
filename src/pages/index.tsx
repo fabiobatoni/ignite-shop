c53c04c372e38655eef0453e2c835c1d238bdbbc
fabiobatoni <fabio_batoni@hotmail.com>
2024-06-30 13:44:42 -0300

    integration with stripe
    Using SSG
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image"
- 
+
 import { useKeenSlider } from "keen-slider/react"
 
-import camiseta1 from '../assets/camisetas/1.png'
 import { HomeContainer, Product } from "../styles/pages/home"
 
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from "../lib/stripe"
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import Stripe from "stripe"
 
 
@@ -47,7 +46,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const response = await stripe.products.list({
     expand: ['data.default_price']
@@ -68,6 +67,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
