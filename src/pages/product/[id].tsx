dc4a973f89996b2b428939c3cbe1bb5c71e0fc79
fabiobatoni <fabio_batoni@hotmail.com>
2024-07-01 09:23:45 -0300

    product & checkout
    Product page structure
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 
 export default function Product() {
@@ -6,6 +7,20 @@ export default function Product() {
   const { query } = useRouter()
 
   return(
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camisa X</h1>
+        <span>R$ 70,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse temporibus incidunt corporis inventore accusantium earum aliquid eius consectetur obcaecati, sit corrupti doloremque sed saepe quae pariatur eaque et. Rerum, blanditiis?</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
