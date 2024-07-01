e8f3eed5d222c40c8c0e82dc63cf74326c43091d
fabiobatoni <fabio_batoni@hotmail.com>
2024-07-01 09:58:00 -0300

    product & checkout
    SSG with dynamic parameter
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,5 @@
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
-import { GetStaticProps } from "next"
+import { GetStaticPaths, GetStaticProps } from "next"
 import { stripe } from "../../lib/stripe"
 import Stripe from "stripe"
 import Image from "next/image"
@@ -34,6 +34,15 @@ export default function Product({ product }: ProductProps) {
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { id: '' } }
+    ],
+    fallback: false,
+  }
 }
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
