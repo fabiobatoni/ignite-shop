87064a6b0ae19bf3a6f0b626ff44b7395a870934
fabiobatoni <fabio_batoni@hotmail.com>
2024-07-01 09:10:40 -0300

    product & checkout
    Navigation with `Link`
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from 'next/link'
 
 import { useKeenSlider } from "keen-slider/react"
 
@@ -32,14 +33,18 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       { products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <Link href={`/product/${product.id}`}  key={product.id} >
+            <Product 
+              className="keen-slider__slide"
+            >
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       }) }
     </HomeContainer>
